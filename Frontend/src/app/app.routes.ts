e27285e40f852ebcbd0a707dc84c3730952a1e23
Mateusz <mati.janusz6@gmail.com>
2026-01-10 00:09:37 +0100

    Add Home Page And Build New Css

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -2,13 +2,19 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './features/login/login.component';
 import { RegisterComponent } from './features/register/register.component';
 import { DashboardComponent } from './features/dashboard/dashboard.component';
-// POPRAWIONY IMPORT (z features, a nie core):
 import { SettingsComponent } from './features/settings/settings.component';
+// 1. IMPORTUJ HOME
+import { HomeComponent } from './features/home/home.component';
 
 export const routes: Routes = [
+  // 2. ZMIEŃ DOMYŚLNĄ ŚCIEŻKĘ: Pusty '' kieruje teraz do HomeComponent
+  { path: '', component: HomeComponent },
+  
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'settings', component: SettingsComponent }, // Trasa do ustawień
-  { path: '', redirectTo: 'login', pathMatch: 'full' }
+  { path: 'settings', component: SettingsComponent },
+  
+  // Opcjonalnie: Jeśli wpisze głupoty, też idź do Home
+  { path: '**', redirectTo: '' }
 ];
